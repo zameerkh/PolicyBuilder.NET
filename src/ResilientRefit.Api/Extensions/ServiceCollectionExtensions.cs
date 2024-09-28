35e983d46791aadc6fbb703a229465d324d1a2c8
zameerkh <zameer.kh@gmail.com>
2024-09-28 15:58:48 +1000

    Unit tests added

--- src/ResilientRefit.Api/Extensions/ServiceCollectionExtensions.cs
+++ src/ResilientRefit.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.OpenApi.Models;
-using Refit;
-using ResilientRefit.Api.Models.ResilientRefit.Api;
+using ResilientRefit.Core;
 using ResilientRefit.Core.Proxy;
 
 namespace ResilientRefit.Api.Extensions;
@@ -11,8 +10,7 @@ public static class ServiceCollectionExtensions
     {
         services.AddControllers();
         services.ConfigureSwagger();
-        services.ConfigurePollyPolicies(configuration);
-        services.ConfigureRefitClient(configuration);
+        services.ConfigureRefitClient<IHttpBinClient>(configuration, "HttpBin");
     }
 
     public static void ConfigureSwagger(this IServiceCollection services)
@@ -34,27 +32,5 @@ public static class ServiceCollectionExtensions
                 options.IncludeXmlComments(xmlPath);
             }
         });
-    }
-
-    public static void ConfigurePollyPolicies(this IServiceCollection services, IConfiguration configuration)
-    {
-        var policyRegistry = services.AddPolicyRegistry();
-        var resiliencySettings = configuration.GetSection("HttpBin:ResiliencyPolicies").Get<ResiliencySettings>();
-
-        var policy = new PolicyBuilder()
-            .WithCircuitBreakerPolicy(resiliencySettings.CircuitBreakerPolicy.Count, TimeSpan.FromSeconds(resiliencySettings.CircuitBreakerPolicy.Duration))
-            .WithRetryPolicy(resiliencySettings.RetryPolicy.Count, resiliencySettings.RetryPolicy.Delay, resiliencySettings.RetryPolicy.Jitter)
-            .WithTimeoutPolicy(TimeSpan.FromSeconds(resiliencySettings.TimeoutPolicy.Duration))
-            .Build();
-
-        policyRegistry.Add("CombinedPolicy", policy);
-    }
-
-    public static void ConfigureRefitClient(this IServiceCollection services, IConfiguration configuration)
-    {
-        var httpBinBaseUrl = configuration["HttpBin:BaseUrl"];
-        services.AddRefitClient<IHttpBinClient>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(httpBinBaseUrl))
-            .AddPolicyHandlerFromRegistry("CombinedPolicy");
     }
 }
